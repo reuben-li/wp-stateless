c872c60f888a6f3f5a8490f1648e1a47f995ed1f
Reuben Li <reuben@spacedotworks.com>
2017-06-18 05:34:46 +0000

    Rewrite URL with static host

--- lib/classes/class-settings.php
+++ lib/classes/class-settings.php
@@ -40,6 +40,7 @@ namespace wpCloud\StatelessMedia {
               'bucket' => get_option( 'sm_bucket' ),
               'root_dir' => get_option( 'sm_root_dir' ),
               'key_json' => get_option( 'sm_key_json' ),
+              'static_host' => get_option( 'sm_static_host' ),
               'body_rewrite' => get_option( 'sm_body_rewrite' ),
               'on_fly' => get_option( 'sm_on_fly' ),
               'delete_remote' => get_option( 'sm_delete_remote' ),
@@ -94,6 +95,7 @@ namespace wpCloud\StatelessMedia {
           'root_dir' => get_option( 'sm_root_dir' ),
           'key_json' => get_option( 'sm_key_json' ),
           'body_rewrite' => get_option( 'sm_body_rewrite' ),
+          'static_host' => get_option( 'sm_static_host' ),
           'on_fly' => get_option( 'sm_on_fly' ),
           'delete_remote' => get_option( 'sm_delete_remote' ),
           'hashify_file_name' => get_option( 'sm_hashify_file_name' ),
@@ -215,6 +217,10 @@ namespace wpCloud\StatelessMedia {
 
         // cache control input
         $inputs[] = '<input id="sm_cache_control" class="regular-text" type="text" name="sm[cache_control]" value="'.$this->get( 'sm.cache_control' ).'" />';
+
+        // use domain as static host
+        $inputs[] = '<input type="hidden" name="sm[static_host]" value="false" />';
+        $inputs[] = '<label for="sm_body_rewrite"><input id="sm_body_rewrite" type="checkbox" name="sm[static_host]" value="true" '. checked( 'true', $this->get( 'sm.static_host' ), false ) .'/>'.__( 'Use bucket as static host.', ud_get_stateless_media()->domain ).'</label>';
 
         // body content rewrite
         $inputs[] = '<input type="hidden" name="sm[body_rewrite]" value="false" />';
